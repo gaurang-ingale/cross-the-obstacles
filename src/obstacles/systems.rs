018d79e6230c368aa69352ecb8800743a25cfb49
Dylan Oldham <dylansoldham@gmail.com>
2024-08-16 10:31:56 -0700

    feat(partial): Obstacles send collision events when hitting player

--- src/obstacles/systems.rs
+++ src/obstacles/systems.rs
@@ -93,17 +93,32 @@ pub fn obstacle_move(
 
 pub fn on_player_hit(
     mut event_writer_player_hit: EventWriter<PlayerHitEvent>,
-    obstacle_query: Query<(&Row), (With<Obstacle>, Without<Player>)>,
-    player_query: Query<(&Row, Entity), (With<Player>, Without<Obstacle>)>,
-    obstacle_image_handle_query: Query<(&Transform, &Handle<Image>), (With<Sprite>, With<Obstacle>, Without<Player>)>,
-    player_image_handle_query: Query<(&Transform, &Handle<Image>), (With<Sprite>, With<Player>, Without<Obstacle>)>
+    obstacle_query: Query<(&Transform, &Handle<Image>, &Row), (With<Obstacle>, Without<Player>)>,
+    player_query: Query<(&Transform, &Handle<Image>, &Row, Entity), (With<Player>, Without<Obstacle>)>,
+    assets: Res<Assets<Image>>
 ) {
-    for (obstacle_row) in obstacle_query.iter() {
-        for (player_row, player_entity) in player_query.iter() {
+    for (obstacle_transform, obstacle_image_handle, obstacle_row) in obstacle_query.iter() {
+        for (player_transform, player_image_handle, player_row, player_entity) in player_query.iter() {
             if obstacle_row != player_row {
                 continue;
             }
             
+            let obstacle_dimensions_uvec = assets.get(obstacle_image_handle).or_else(|| { println!("Failed to find obstacle image"); None }).unwrap().size();
+            let player_dimensions_uvec = assets.get(player_image_handle).or_else(|| { println!("Failed to find obstacle image"); None }).unwrap().size();
+
+            let obstacle_dimensions = Vec2::new(obstacle_dimensions_uvec.x as f32, obstacle_dimensions_uvec.y as f32);
+            let player_dimensions = Vec2::new(player_dimensions_uvec.x as f32, player_dimensions_uvec.y as f32);
+
+            let obstacle_bounding_box = Rect::from_center_size(obstacle_transform.translation.truncate(), 
+                obstacle_dimensions * obstacle_transform.scale.truncate()
+            );
+            let player_bounding_box = Rect::from_center_size(player_transform.translation.truncate(),
+                player_dimensions * player_transform.scale.truncate()
+            );
+
+            if !player_bounding_box.intersect(obstacle_bounding_box).is_empty() {
+                event_writer_player_hit.send(PlayerHitEvent{player_entity});
+            }
         }
     }
 }
